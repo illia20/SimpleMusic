460e7fb41ea25e73271eb017c309ca9c146da19d
Illia D <illiad123321@gmail.com>
2020-09-14 18:09:42 +0300

    new api

---
+++ SimpleMusic/api/ComposersController.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SimpleMusic.Models;
+
+namespace SimpleMusic.api
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ComposersController : ControllerBase
+    {
+        private readonly MusicDbContext _context;
+
+        public ComposersController(MusicDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Composers
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Composer>>> GetComposers()
+        {
+            return await _context.Composers.ToListAsync();
+        }
+
+        // GET: api/Composers/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Composer>> GetComposer(int id)
+        {
+            var composer = await _context.Composers.FindAsync(id);
+
+            if (composer == null)
+            {
+                return NotFound();
+            }
+
+            return composer;
+        }
+
+        // PUT: api/Composers/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutComposer(int id, Composer composer)
+        {
+            if (id != composer.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(composer).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ComposerExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Composers
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPost]
+        public async Task<ActionResult<Composer>> PostComposer(Composer composer)
+        {
+            _context.Composers.Add(composer);
+            await _context.SaveChangesAsync();
+
+            return Ok(composer);
+        }
+
+        // DELETE: api/Composers/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Composer>> DeleteComposer(int id)
+        {
+            var composer = await _context.Composers.FindAsync(id);
+            if (composer == null)
+            {
+                return NotFound();
+            }
+
+            _context.Composers.Remove(composer);
+            await _context.SaveChangesAsync();
+
+            return composer;
+        }
+
+        private bool ComposerExists(int id)
+        {
+            return _context.Composers.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ SimpleMusic/api/PiecesController.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SimpleMusic.Models;
+
+namespace SimpleMusic.api
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PiecesController : ControllerBase
+    {
+        private readonly MusicDbContext _context;
+
+        public PiecesController(MusicDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Pieces
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Piece>>> GetPieces()
+        {
+            return await _context.Pieces.ToListAsync();
+        }
+
+        // GET: api/Pieces/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Piece>> GetPiece(int id)
+        {
+            var piece = await _context.Pieces.FindAsync(id);
+
+            if (piece == null)
+            {
+                return NotFound();
+            }
+
+            return piece;
+        }
+
+        // PUT: api/Pieces/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutPiece(int id, Piece piece)
+        {
+            if (id != piece.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(piece).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!PieceExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Pieces
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPost]
+        public async Task<ActionResult<Piece>> PostPiece(Piece piece)
+        {
+            if (PieceExists(piece))
+                return BadRequest("Error");
+            _context.Pieces.Add(piece);
+            await _context.SaveChangesAsync();
+
+            return Ok(piece);
+        }
+
+        // DELETE: api/Pieces/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Piece>> DeletePiece(int id)
+        {
+            var piece = await _context.Pieces.FindAsync(id);
+            if (piece == null)
+            {
+                return NotFound();
+            }
+
+            _context.Pieces.Remove(piece);
+            await _context.SaveChangesAsync();
+
+            return piece;
+        }
+
+        private bool PieceExists(int id)
+        {
+            return _context.Pieces.Any(e => e.Id == id);
+        }
+        private bool PieceExists(Piece piece)
+        {
+            return _context.Pieces.Any(e => e.Name == piece.Name && e.Composer == piece.Composer);
+        }
+    }
+}
